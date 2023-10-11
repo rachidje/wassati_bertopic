05c549fdf97be8d911e12433fc69134181c4e4c3
Rachid Jeffali <rachid.jeffali@wassati.com>
2023-10-12 01:06:49 +0200

    mis a a jour du streamlit_app.py et fixer les bugs d'affichage

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,12 @@
-import streamlit as st
-import numpy as np
-import pandas as pd
-import streamlit.components.v1 as components
-from utils.load_streamlit_data import data
+import os
 import yaml
-from yaml.loader import SafeLoader
+import numpy as np
+import streamlit as st
 import streamlit_authenticator as stauth
+import streamlit.components.v1 as components
+
+from yaml.loader import SafeLoader
+from utils.load_streamlit_data import data
 
 st.set_page_config(layout="wide")
 
@@ -25,20 +26,32 @@ if'key' not in st.session_state:
 
 name, authentication_status, username = authenticator.login('Login', 'main')
 
+def print_graph(path, height=None, width=None):
+    """
+    Prints a graph from a specified file path.
+
+    Parameters:
+        path (str): The file path of the graph.
+        height (int, optional): The height of the graph. Defaults to None.
+        width (int, optional): The width of the graph. Defaults to None.
+
+    Returns:
+        None
+    """
+    graph = open(path)
+    # get the extension
+    extension = os.path.splitext(path)[1]
+    if extension==".html":
+        return components.html(graph.read(), height=height, width=width)
+    elif extension==".png":
+        return st.image(path)
 
 def main():
-    # p = open("data/graphs/topic_merged_per_class_sentiment.html")
-    # components.html(p.read())
     st.title("Example of Verbatim Analysis")
-    
-    
-    def print_graph_html(path, height=None, width=None):
-        graph = open(path)
-        return components.html(graph.read(), height=height, width=width)
     
     groupby_options = ['year', 'Zone', 'Clusters','Account Country', 'Market Segment']
     # Store the parameters lists in a dictionary
-    my_data = {option: (np.insert(data[option].unique().astype('object'), 0, "all time") if option == 'year' else data[option].unique()) for option in groupby_options}
+    my_data = {option: (np.insert(data[option].unique().astype('object'), 0, "all_time") if option == 'year' else data[option].unique()) for option in groupby_options}
     # Add the merged_topics and emotions lists to the my_data dictionary
     my_data['merged_topics'] = data['label'].unique()
     my_data['emotions'] = data['single_emotion_label'].unique()
@@ -68,15 +81,15 @@ def main():
     col1, col2 = st.columns([1,1.7])
     with col1:
         st.markdown("<h3 style='text-align: center; color: black;'>Many very precise topics</h3>", unsafe_allow_html=True)
-        print_graph_html('data/graphs/Clustering/global/topic_visualize_topics.html', height=750)
+        print_graph('data/graphs/Clustering/global/topic_visualize_topics.html', height=750)
     with col2:
         st.markdown("<h3 style='text-align: center; color: black;'>The biggest 12 topics keywords</h3>", unsafe_allow_html=True)
-        print_graph_html('data/graphs/Clustering/global/top_12_topics_barchart_viz.html', height=750)
+        print_graph('data/graphs/Clustering/global/top_12_topics_barchart_viz.html', height=750)
     
     
     st.subheader('''Topics hierarchy''')
     st.write("""Let's ordered all those topics hierarchicaly\n\n""")
-    print_graph_html('data/graphs/Clustering/hierarchy/topics_hierarchy.html', height=1050)
+    print_graph('data/graphs/Clustering/hierarchy/topics_hierarchy.html', height=1050)
     
     
     st.subheader('Aggregated Topics')
@@ -98,7 +111,7 @@ def main():
     
     st.subheader('''Vizualize documents per aggregated topic''')
     st.write("""Let's regroup the many subtopics into the aggregated topics\n\n""")
-    print_graph_html('data/graphs/Clustering/documents_viz/topic_merged_visualize_reduced_docs.html', height=750)
+    print_graph('data/graphs/Clustering/documents_viz/topic_merged_visualize_reduced_docs.html', height=750)
     
     
     st.subheader('''Wordcloud''')
@@ -117,7 +130,7 @@ def main():
     
     st.subheader('''Topic Evolution''')
     st.write("""Let's check the topics evolution in time (by months)\n\n""")
-    print_graph_html('data/graphs/Clustering/topic_in_time/topic_merged_time_by_months.html', height=500)
+    print_graph('data/graphs/Clustering/topic_in_time/topic_merged_time_by_months.html', height=500)
     
     
     st.subheader('''Heatmaps Graphics''')
@@ -126,10 +139,10 @@ def main():
     col1, col2 = st.columns(2)
     with col1:
         st.markdown("<h4 style='text-align: center; color: grey;'>The global topics relations</h4>", unsafe_allow_html=True)
-        print_graph_html('data/graphs/Clustering/heatmap/topic_merged_heatmap.html', height=750)
+        print_graph('data/graphs/Clustering/heatmap/topic_merged_heatmap.html', height=750)
     with col2:
         st.markdown("<h4 style='text-align: center; color: grey;'>The sub-topics relations</h4>", unsafe_allow_html=True)
-        print_graph_html('data/graphs/Clustering/heatmap/topic_heatmap.html', height=750)
+        print_graph('data/graphs/Clustering/heatmap/topic_heatmap.html', height=750)
     
     
     st.subheader("Topic Repartition")
@@ -137,7 +150,7 @@ def main():
     # Giving user options for selecting the class repartition
     groupby_option = st.selectbox('Select group : by which class do you want to see the topic repartition?',groupby_options)
     # Produces topic_per_class barchart
-    print_graph_html(f'data/graphs/Clustering/topic_repartition/by_{groupby_option}/model_merged_per_{groupby_option}.html', height=750)
+    print_graph(f'data/graphs/Clustering/topic_repartition/by_{groupby_option}/model_merged_per_{groupby_option}.html', height=750)
     
     
     st.header("Sentiment Analysis")
@@ -151,10 +164,10 @@ def main():
     col1, col2 = st.columns([1,2.3])
     with col1:
         st.markdown("<h4 style='text-align: center; color: grey;'>By Sentiment</h4>", unsafe_allow_html=True)
-        print_graph_html('data/graphs/topics_per_class/topic_model_merged/sentiment.html', height=750)
+        print_graph('data/graphs/Sentiment_Analysis/by_sentiment/repartition_per_topic/global/model_merged_per_sentiment.html', height=750)
     with col2:
         st.markdown("<h4 style='text-align: center; color: grey;'>By Emotion</h4>", unsafe_allow_html=True)
-        print_graph_html('data/graphs/topics_per_class/topic_model_merged/emotion.html', height=750)
+        print_graph('data/graphs/Sentiment_Analysis/by_emotion/repartition_per_topic/global/model_merged_per_emotion.html', height=750)
     
     
     st.subheader('Single emotion repartition')
@@ -166,73 +179,71 @@ def main():
     # We define 3 columns in order to put the image in the second and then have it centered
     if groupby_option=='Zone' or groupby_option=='Clusters' or groupby_option=='Account Country' or groupby_option=='Market Segment' :
         time_period = st.selectbox('Select the period of time you want to study',my_data["year"])
-        col1, col2, col3 = st.columns([1,3,1])
-        # case where "all time" is chosen, the files weren't saved with the "all time" suffix -> Could be updated in the image generation part
-        if time_period == "all time":
-            with col2:
-                st.image(f'data/graphs/emotions/{emotion}_by_{groupby_option}.png')
-        # any other case where time_period isn't "all time"
-        else:
-            with col2:
-                st.image(f'data/graphs/emotions/{emotion}_by_{groupby_option}_{time_period}.png')
-    
-    elif groupby_option=="year":
-        col1, col2, col3 = st.columns([1,3,1])
+        _, col2, _ = st.columns([1,3,1])
         with col2:
-            st.image(f'data/graphs/emotions/{emotion}_by_{groupby_option}.png') 
+            path = f'data/graphs/Sentiment_Analysis/by_emotion/repartition/by_{groupby_option}/{emotion}_{time_period}.png'
+            print_graph(path, height=750)
+
+    elif groupby_option=="year":
+        _, col2, _ = st.columns([1,3,1])
+        with col2:
+            path = f'data/graphs/Sentiment_Analysis/by_emotion/repartition/by_{groupby_option}/{emotion}.png'
+            print_graph(path, height=750)
     
     
     
-    texte = """Go deeper in the evolution of each emotion. 
-    For that you will find below two barcharts.\n
-    One represents the repartition of a specified emotion within a group of your choice (like geographical filters, market segment filter or the repartition in time.\n
-    The other chart is more precise, more specific. It still represents the repartition of a specified emotion within a group of your choice, adding one more information : the topics. You can through it study the repartition of an emotion per topic and per a group of your choice\n\n"""
-    st.markdown(f"<h3 style='text-align: left; color: black;'>{texte}</h3>", unsafe_allow_html=True)
+    st.markdown(f"<h2 style='text-align: left; color: black;'>Go deeper in the evolution of each emotion.</h2>", unsafe_allow_html=True)
+    st.write(
+        """ 
+        For that you will find below two barcharts.\n
+        One represents the repartition of a specified emotion within a group of your choice (like geographical filters, market segment filter or the repartition in time.\n
+        The other chart is more precise, more specific. It still represents the repartition of a specified emotion within a group of your choice, adding one more information : the topics. You can through it study the repartition of an emotion per topic and per a group of your choice\n\n"""
+    )
     
     st.subheader("Repartition of the emotions within a subclass")
     st.write("""Here we can track the repartition of a specific emotion according to a specified data group.\n\n""")
     emotion = st.selectbox('Which emotion do you want to see the repartition ?',my_data["emotions"])
     groupby_option = st.selectbox('With which class do you want to see the topic repartition of the emotion chosen?',groupby_options)
-    print_graph_html(f'data/graphs/topics_per_class/topic_model_merged/per_{shorter_names[groupby_option]}/for_emotion/{emotion}.html', height=750)
+    print_graph(f'data/graphs/Sentiment_Analysis/by_emotion/repartition_per_topic/emotion_by_class/by_{groupby_option}/{emotion}.html', height=750)
     
     
     st.subheader("Emotion Repartition according to a subclass AND the topics")
     st.write("""Let's check the repartition of a specific emotion according to a specified data group AND according to the topics.\n\n""")
     groupby_option = st.selectbox('By which class do you want to see the emotion topic repartition?',groupby_options)
     # Propose to the user to choose one of the values that exist in his precedent chosen class
-    options = [x for x in my_data[groupby_option] if groupby_option != "year" or x != "all time"]
+    options = [x for x in my_data[groupby_option] if groupby_option != "year" or x != "all_time"]
     value = st.selectbox(f'By which {shorter_names[groupby_option]} do you want to see the emotion topic repartition?',options)
     
-    print_graph_html(f'data/graphs/topics_per_class/topic_model_merged/per_emotion/for_{shorter_names[groupby_option]}/{value}.html', height=750)
+    print_graph(f'data/graphs/Sentiment_Analysis/by_emotion/repartition_per_topic/class_by_emotions/by_{groupby_option}/{value}.html', height=750)
     
     
     # Custom footer workaround to overide default streamlit footer
-    footer = """<style>
-    a:link , a:visited{
-    color: blue;
-    background-color: transparent;
-    text-decoration: underline;
-    }
-    a:hover,  a:active {
-    color: red;
-    background-color: transparent;
-    text-decoration: underline;
-    }
-    .footer {
-    position: fixed;
-    left: 0;
-    bottom: 0;
-    width: 100%;
-    background-color: white;
-    color: black;
-    text-align: center;
-    }
-    </style>
-    <div class="footer">
-    <p>Model constructed by <a href="https://wassati.com" >Wassati - Engaging Organizations </a></p>
-    </div>
-    """
-    st.markdown(footer, unsafe_allow_html=True)
+    footer = '''<style>
+            a:link , a:visited{
+                color: blue;
+                background-color: transparent;
+                text-decoration: underline;
+            }
+            a:hover,  a:active {
+                color: red;
+                background-color: transparent;
+                text-decoration: underline;
+            }
+            .footer {
+                position: fixed;
+                left: 0;
+                bottom: 0;
+                width: 100%;
+                background-color: white;
+                color: black;
+                text-align: center;
+            }
+            </style>
+            <div class="footer">
+                <p>Model constructed by <a href="https://wassati.com" >Wassati - Engaging Organizations </a></p>
+            </div>
+            '''
+    components.html(footer)
 
 
 if authentication_status:
